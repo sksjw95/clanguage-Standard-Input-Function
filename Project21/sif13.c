ec2a9ff5711dcd50b045ec83b8a2a2349f5de70c
sksjw95 <tjwldnd95@hanmail.net>
2023-08-09 01:21:07 +0900

    scanf_s 연습문제

---
+++ Project21/sif13.c
@@ -0,0 +1,11 @@
+// 정수 2개를 더한 후 그 값을 출력
+#include<stdio.h>
+void main() {
+	int data1;
+	int data2;
+	
+
+	scanf_s("%d %d",&data1,&data2);
+	printf("%d + %d = %d\n", data1, data2, data1 + data2);
+	
+}
